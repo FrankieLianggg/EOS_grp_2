233f1ee5cb53d84a26f3d8b3116986052c7abc68
Ws778899 <wangshuai5154@gmail.com>
2026-04-14 23:01:03 -0400

    Create DbConfig.java

---
+++ project2/JDBC-Class-Library-Project/src/main/java/com/eosgrp/jdbc/config/DbConfig.java
@@ -0,0 +1,9 @@
+package com.eosgrp.jdbc.config;
+
+public class DbConfig {
+    public static final String URL =
+        "jdbc:sqlserver://localhost:1433;databaseName=Northwinds2024Student";
+
+    public static final String USER = "sa";
+    public static final String PASSWORD = "your_password";
+}
