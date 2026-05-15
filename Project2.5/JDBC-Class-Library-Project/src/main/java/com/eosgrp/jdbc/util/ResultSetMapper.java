749541cc8261f6e4a1643a914be7187755fcaa7a
FrankieLianggg <Frankieliang007@gmail.com>
2026-05-14 23:53:26 -0400

    Not required for Project 2.5

--- Project2.5/JDBC-Class-Library-Project/src/main/java/com/eosgrp/jdbc/util/ResultSetMapper.java
+++
@@ -1,9 +0,0 @@
-package com.eosgrp.jdbc.util;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-@FunctionalInterface
-public interface ResultSetMapper<T> {
-    T map(ResultSet rs) throws SQLException;
-}
